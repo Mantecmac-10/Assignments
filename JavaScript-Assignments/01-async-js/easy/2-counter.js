efbaea4bbcce92683ca56158b10361aafe6cd22a
Mantecmac-10 <Mantech468@gmail.com>
2026-03-08 22:23:57 +0530

    js-assignment async easy

--- JavaScript-Assignments/01-async-js/easy/2-counter.js
+++ JavaScript-Assignments/01-async-js/easy/2-counter.js
@@ -3,3 +3,11 @@
 // Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
 
 // (Hint: setTimeout)
+
+
+let counter = 0
+
+setTimeout(()=>{
+    counter++
+    console.log(counter)
+},1000)
